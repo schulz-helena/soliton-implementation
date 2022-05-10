da3ae43bf88db394fcc3a5c8836e7c843513d563
helschulz <helschulz@uni-potsdam.de>
2022-05-10 20:40:46 +0200

    bindings_list as an additional argument for init

--- soliton_path.py
+++ soliton_path.py
@@ -15,14 +15,23 @@ class SolitonPath:
 
     path: list
     soliton_graph: SolitonGraph
+    bindings_list: list
+
+    #def __post_init__(self):
+        #self.bindings_list = self.find_bindings_for_each_timestep()
+        #self.adjacency_matrices_list = self.find_adjacency_matrices_for_each_timestep()
 
     def __post_init__(self):
-        self.bindings_list = self.find_bindings_for_each_timestep()
         self.adjacency_matrices_list = self.find_adjacency_matrices_for_each_timestep()
 
 
     def find_bindings_for_each_timestep(self):
-        soliton_graph_copy = copy.deepcopy(self.soliton_graph) #working on a copy of the soliton graph so no unwanted changes occur
+        """Find the binding dict for each timestep of traversal of a path
+
+        Returns:
+            list: contains dict for each timestep
+        """
+        soliton_graph_copy = copy.deepcopy(self.soliton_graph) # working on a copy of the soliton graph so no unwanted changes occur
         bindings_list = []
         bindings_copy = soliton_graph_copy.bindings.copy()
         bindings_list.append(bindings_copy)
@@ -44,18 +53,27 @@ class SolitonPath:
 
 
     def find_adjacency_matrices_for_each_timestep(self):
+        """Find the adjacency matrix for each timestep of traversal of a path
+
+        Returns:
+            list: contains matrix for each timestep
+        """
         soliton_graph_copy = copy.deepcopy(self.soliton_graph)
         adjacency_matrices_list = []
         for i in range(0, len(self.bindings_list)):
-            bindings = self.bindings_list[i]
-            soliton_graph_copy.set_bindings(bindings) #change bindings in graph which changes edge weights which changes adjacency matrix
+            bindings = self.bindings_list[i] # binding dict for that timestep
+            soliton_graph_copy.set_bindings(bindings) # change bindings in graph which changes edge weights which changes adjacency matrix
             matrix = nx.to_numpy_matrix(soliton_graph_copy.graph)
             adjacency_matrices_list.append(matrix)
         return adjacency_matrices_list
 
 
 if __name__ == "__main__":
-    my_graph = SolitonGraph('CC=CC')
-    my_path = SolitonPath([0,1,2,3], my_graph)
+    
+    #my_graph = SolitonGraph('CC=CC')
+    #my_path = SolitonPath([0,1,2,3], my_graph)
+    my_graph = SolitonGraph('C1{1}=C{3}C1{=2}')
+    my_bindings_list = [{(0, 1): 1, (0, 2): 2, (0, 4): 1, (2, 3): 1, (2, 4): 1, (4, 5): 2}, {(0, 1): 2, (0, 2): 2, (0, 4): 1, (2, 3): 1, (2, 4): 1, (4, 5): 2}, {(0, 1): 2, (0, 2): 1, (0, 4): 1, (2, 3): 1, (2, 4): 1, (4, 5): 2}, {(0, 1): 2, (0, 2): 1, (0, 4): 1, (2, 3): 1, (2, 4): 2, (4, 5): 2}, {(0, 1): 2, (0, 2): 1, (0, 4): 1, (2, 3): 1, (2, 4): 2, (4, 5): 1}]
+    my_path = SolitonPath([1, 0, 2, 4, 5], my_graph, my_bindings_list)
     print(my_path.bindings_list)
     print(my_path.adjacency_matrices_list)
