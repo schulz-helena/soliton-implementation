413b1dd659225b60c3244811f6ffb7b56f1efe99
helschulz <helschulz@uni-potsdam.de>
2022-08-23 12:30:58 +0200

    try with only parent package

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     author = 'Helena Schulz',
     author_email = 'schulz-helena@gmx.de',
     url = 'https://github.com/schulz-helena/soliton-implementation',
-    packages = ['mini_soliton_automata', 'gui', 'res', 'soliton_classes', 'visualisations'],
+    packages = ['mini_soliton_automata'],
     python_requires = '>3.9',
     install_requires = [
         'rdkit>=2021.09.5',
